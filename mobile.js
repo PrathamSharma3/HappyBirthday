aa351cb0732a2308ec79e0d97f55916962639ba8
PrathamSharma3 <jeremy777988@gmail.com>
2025-07-22 10:19:24 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -2,12 +2,9 @@ let highestZ = 1;
 
 class Paper {
   holdingPaper = false;
+  rotating = false;
   touchStartX = 0;
   touchStartY = 0;
-  touchMoveX = 0;
-  touchMoveY = 0;
-  touchEndX = 0;
-  touchEndY = 0;
   prevTouchX = 0;
   prevTouchY = 0;
   velX = 0;
@@ -15,74 +12,57 @@ class Paper {
   rotation = Math.random() * 30 - 15;
   currentPaperX = 0;
   currentPaperY = 0;
-  rotating = false;
 
   init(paper) {
+    // Touch start
+    paper.addEventListener('touchstart', (e) => {
+      if (e.touches.length === 1) {
+        this.holdingPaper = true;
+        this.rotating = false;
+        paper.style.zIndex = highestZ++;
+        this.touchStartX = e.touches[0].clientX;
+        this.touchStartY = e.touches[0].clientY;
+        this.prevTouchX = this.touchStartX;
+        this.prevTouchY = this.touchStartY;
+      } else if (e.touches.length === 2) {
+        this.rotating = true;
+        this.holdingPaper = false;
+        // Optionally, store initial angle here for custom rotation logic
+      }
+    });
+
+    // Touch move
     paper.addEventListener('touchmove', (e) => {
       e.preventDefault();
-      if(!this.rotating) {
-        this.touchMoveX = e.touches[0].clientX;
-        this.touchMoveY = e.touches[0].clientY;
-        
-        this.velX = this.touchMoveX - this.prevTouchX;
-        this.velY = this.touchMoveY - this.prevTouchY;
+      if (e.touches.length === 2) {
+        this.rotating = true;
+        // Optionally, implement custom rotation logic here
+        return;
       }
-        
-      const dirX = e.touches[0].clientX - this.touchStartX;
-      const dirY = e.touches[0].clientY - this.touchStartY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
-      const dirNormalizedX = dirX / dirLength;
-      const dirNormalizedY = dirY / dirLength;
-
-      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
-      let degrees = 180 * angle / Math.PI;
-      degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
-        this.rotation = degrees;
-      }
-
-      if(this.holdingPaper) {
-        if(!this.rotating) {
-          this.currentPaperX += this.velX;
-          this.currentPaperY += this.velY;
-        }
-        this.prevTouchX = this.touchMoveX;
-        this.prevTouchY = this.touchMoveY;
-
+      if (this.holdingPaper && e.touches.length === 1) {
+        const touchX = e.touches[0].clientX;
+        const touchY = e.touches[0].clientY;
+        this.velX = touchX - this.prevTouchX;
+        this.velY = touchY - this.prevTouchY;
+        this.currentPaperX += this.velX;
+        this.currentPaperY += this.velY;
+        this.prevTouchX = touchX;
+        this.prevTouchY = touchY;
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
-    })
+    }, { passive: false });
 
-    paper.addEventListener('touchstart', (e) => {
-      if(this.holdingPaper) return; 
-      this.holdingPaper = true;
-      
-      paper.style.zIndex = highestZ;
-      highestZ += 1;
-      
-      this.touchStartX = e.touches[0].clientX;
-      this.touchStartY = e.touches[0].clientY;
-      this.prevTouchX = this.touchStartX;
-      this.prevTouchY = this.touchStartY;
-    });
-    paper.addEventListener('touchend', () => {
-      this.holdingPaper = false;
-      this.rotating = false;
-    });
-
-    // For two-finger rotation on touch screens
-    paper.addEventListener('gesturestart', (e) => {
-      e.preventDefault();
-      this.rotating = true;
-    });
-    paper.addEventListener('gestureend', () => {
-      this.rotating = false;
+    // Touch end
+    paper.addEventListener('touchend', (e) => {
+      if (e.touches.length === 0) {
+        this.holdingPaper = false;
+        this.rotating = false;
+      }
     });
   }
 }
 
 const papers = Array.from(document.querySelectorAll('.paper'));
-
 papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
